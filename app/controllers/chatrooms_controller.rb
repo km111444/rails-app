1c8c4c7fa5e1f7d45f74b8bb84f4157f457ed63a
Kanae Maki <m14ky.gu@gmail.com>
2022-03-24 14:07:52 +0900

    chat model new

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,9 @@
+class ChatroomsController < ApplicationController
+  # def index
+  #   @chatrooms = Chatroom.where(user_id == 1)
+  # end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,6 @@ puts "creating pharmacies...."
 
 User.create(name: "田中花子", phone_number: 1111111111)
 Pharmacy.create(address: "5-7-18, ogikubo, suginami, tokyo", pharmacy_name: "明弘堂薬局", user: User.first)
+Chatroom.create(chatroom_name: "general", user_id: 1)
 
 puts "done"
