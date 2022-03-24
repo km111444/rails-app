194dcf88ce73b2154f2dbb12ca0fdedea1aaa4c1
Kanae Maki <m14ky.gu@gmail.com>
2022-03-24 23:54:41 +0900

    broadcasting

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,5 +7,10 @@ export default class extends Controller {
 
   connect() {
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+    this.channel = consumer.subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => console.log(data) }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
