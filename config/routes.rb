37526d953e92892c4ae4b9763c2625b6cbf2ba68
Kanae Maki <m14ky.gu@gmail.com>
2022-04-01 17:38:44 +0900

    prescription controller new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pharmacies
+  resources :pharmacies do
+    resources :prescriptions, only: [:index, :show]
+  end
   resources :chatrooms, only: [:index, :show] do
     resources :messages, only: :create
   end
